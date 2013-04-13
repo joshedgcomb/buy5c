09253da77a95e9a1e027be0def1e3b04a81feec9
Josh Edgcomb <joshedgcomb@yahoo.com>
2013-04-13 03:30:46 -0700

    everything so far

--- app.py
+++ app.py
@@ -8,7 +8,8 @@ from flask import (
     request,
     url_for,
     g,
-    flash
+    flash,
+    escape
 )
 import db
 import urllib
@@ -16,21 +17,34 @@ import requests
 import json
 import datetime
 from flask.ext.mongokit import MongoKit, Document
-from flask.ext.login import (
-    LoginManager,
-    login_user,
-    logout_user,
-    current_user
-)
+from flask.ext.login import (LoginManager, current_user, login_required,
+                            login_user, logout_user, UserMixin, AnonymousUser,
+                            confirm_login, fresh_login_required)
 
 import re
+
+
+USERS = {
+    
+}
 
 
 app = Flask(__name__)
 
 login_manager = LoginManager()
+login_manager.login_view = "login_redirect"
+login_manager.login_message = u"Please log in to access this page."
+login_manager.refresh_view = "reauth"
+app.config.from_object(__name__)
+
+
+@login_manager.user_loader
+def load_user(id):
+    for x in db.users.find():
+        if x['username'] == id:
+            return User(x['username'], x['email'], x['password'], x['active'])
+
 login_manager.setup_app(app)
-login_manager.login_view = 'login'
 
 db = MongoKit(app)
 
@@ -38,9 +52,10 @@ db = MongoKit(app)
 app.secret_key = 'fuck you this is my secret key'
 
 
-@login_manager.user_loader
-def load_user(userid):
-    return User.get(userid)
+@app.route('/new_account')
+def new_account():
+    return render_template('new_account.html')
+
 
 
 class User_Account(Document):
@@ -49,30 +64,37 @@ class User_Account(Document):
         'username': basestring,
         'email': basestring,
         'password': basestring,
+        'active': bool
     }
+
+
+
+class User():
+    def __init__(self, username, email, password, active):
+        self.username = username
+        self.email = email
+        self.password = password
+        self.active = active
+
+    def is_active(self):
+        return self.active
+
+
+    def is_anonymous(self):
+        return False
+
+
+    def is_authenticated(self):
+        return True
+
+
+    def get_id(self):
+        return self.username
 
     required_fields = ['username', 'email', 'password']
     use_dot_notation = True
 
 db.register([User_Account])
-
-@app.route('/create_account', methods=['GET', 'POST'])
-def create_account():
-    if request.method == 'POST':
-        account = db.User_Account
-        account.username = request.form['username']
-        account.email = request.form['email']
-        account.password = request.form['password']
-        account.save()
-        return 'john is a poopyhead'
-    return render_template(url_for('index'))
-
-@app.route('/new_account')
-def new_account():
-    return render_template('new_account.html')
-
-
-
 
 pomona_regex = re.compile(r"[\w.]+@[\w.]*pomona.edu")
 cmc_regex = re.compile(r"[\w.]+@[\w.]*cmc.edu")
@@ -80,32 +102,38 @@ hmc_regex = re.compile(r"[\w.]+@[\w.]*hmc.edu")
 scripps_regex = re.compile(r"[\w.]+@[\w.]*scripps.edu")
 pitzer_regex = re.compile(r"[\w.]+@[\w.]*pitzer.edu")
 
+@app.route('/create_account', methods=['GET', 'POST'])
+def create_account():
+    if request.method == 'POST':
+        email = request.form['email']
+        username = request.form['username']
+        password = request.form['password']
 
 
-'''class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    nickname = db.Column(db.String(64), unique=True)
-    email = db.Column(db.String(120), unique=True)
-    role = db.Column(db.SmallInteger, default=ROLE_USER)
-    posts = db.relationship('Post', backref='author', lazy='dynamic')
+        pomona_match = pomona_regex.match(email)
+        hmc_match = hmc_regex.match(email)
+        cmc_match = cmc_regex.match(email)
+        pitzer_match = pitzer_regex.match(email)
+        scripps_match = scripps_regex.match(email)
 
-    def is_authenticated(self):
-        return True
+        if ((pomona_match is None) and (hmc_match is None) and
+            (cmc_match is None) and (pitzer_match is None) and
+            (scripps_match is None)):
+            return 'you done fucked up'
 
-    def is_active(self):
-        return True
 
-    def is_anonymous(self):
-        return False
+        
 
-    def get_id(self):
-        return unicode(self.id)
+        if (db.users.find_one(username) is None):
+            account = db.User_Account
+            account.username = username
+            account.email = email
+            account.password = password
+            account.active = False
+            account.save()
+        return 'john is a poopyhead'
+    return render_template(url_for('index'))
 
-    def __repr__(self):
-        return '<User %r>' % (self.nickname)
-        '''
-
-YELP_SEARCH_URL = 'http://api.yelp.com/v2/search'
 
 
 @app.before_request
@@ -113,60 +141,48 @@ def before_request():
     g.user = current_user
 
 
-@app.route('/login', methods = ['GET', 'POST'])
+@app.route('/login',)
 def login():
-    if g.user is not None and g.user.is_authenticated():
-        return redirect(url_for('index'))
-    form = LoginForm()
-    if form.validate_on_submit():
-        session['remember_me'] = form.remember_me.data
-        #!!!return oid.try_login(form.openid.data, ask_for=['nickname', 'email'])
-    return render_template('login.html',
-                           title='Sign In',
-                           form=form,
-                           providers=app.config['OPENID_PROVIDERS'])
+    return render_template('login.html')
 
 
-#!!!@oid.after_login
-def after_login(resp):
-    if resp.email is None or resp.email == "":
-        flash('Invalid login. Please try again.')
-        redirect(url_for('login'))
-    user = User.query.filter_by(email=resp.email).first()
-    if user is None:
-        nickname = resp.nickname
-        if nickname is None or nickname == "":
-            nickname = resp.email.split('@')[0]
-        user = User(nickname=nickname, email=resp.email, role=ROLE_USER)
-        db.session.add(user)
-        db.session.commit()
-    remember_me = False
-    if 'remember_me' in session:
-        remember_me = session['remember_me']
-        session.pop('remember_me', None)
-    login_user(user, remember=remember_me)
-    return redirect(request.args.get('next') or url_for('index'))
+@app.route('/login_redirect', methods=['GET', 'POST'])
+def login_redirect():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        session['username'] = username
+        for x in db.users.find():
+            if x['username'] == username and x['password'] == password:
+                x['active'] = True
+                user = User(x['username'], x['email'], x['password'], x['active'])
+                USERS[username] = user
+                return str(login_user(user))
+
+    return 'login failed'
+
+
+
+@app.route("/logout")
+def logout():
+    session.pop('username', None)
+    logout_user()
+    flash("Logged out.")
+    return redirect('/index')
+
+@login_manager.user_loader
+def load_user(id):
+    return False
+
 
 
 @app.route('/')
 @app.route('/index')
-#!!!@login_required
 def index():
-    user = g.user
-    posts = [
-        {
-            'author': {'nickname': 'John'}, 
-            'body': 'Beautiful day in Portland!'
-        },
-        {
-            'author': {'nickname': 'Susan'},
-            'body': 'The Avengers movie was so cool!'
-        }
-    ]
-    return render_template('index.html',
-                           title='Home',
-                           user=user,
-                           posts=posts)
+    if 'username' in session:
+        return render_template('index.html',
+                               username=escape(session['username']))
+    return render_template('index.html')
 
 
 @app.route('/results', methods=['POST'])
@@ -190,17 +206,49 @@ def results():
                            businesses=json_response['businesses'])
 
 
-@app.route('/save', methods=["POST"])
-def save():
-    new_entry = collection.Entry()
-    new_entry.name = request.form['name']
-    new_entry.url = request.form['url']
-    new_entry.phone_number = request.form['phone_number']
-    new_entry.address = request.form['address']
-    new_entry.categories = request.form.getlist('categories')
 
-    new_entry.save()
+@app.route('/a_a')
+def a_a():
+    return False
 
-    return redirect('/')
+
+@app.route('/appliances')
+def appliances():
+    return False
+
+
+@app.route('/books')
+def books():
+    return False
+
+
+@app.route('/electronics')
+def electronics():
+    return False
+
+
+@app.route('/furniture')
+def furniture():
+    return False
+
+
+@app.route('/mmg')
+def mmg():
+    return False
+
+
+@app.route('/tickets')
+def tickets():
+    return False
+
+
+@app.route('/other')
+def other():
+    return False
+
+@app.route('/listing')
+def listing():
+    return False
+
 if __name__ == '__main__':
     app.run(debug=True)
